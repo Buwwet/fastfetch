770268dda7afab334e1fa46697620c5f4f34cdb8
李通洲 <zhangsongcui3371@sina.com>
2024-07-17 10:56:30 +0800

    CPU: support display frequency in MHz

--- src/detection/cpu/cpu_bsd.c
+++ src/detection/cpu/cpu_bsd.c
@@ -61,10 +61,9 @@ const char* ffDetectCPUImpl(const FFCPUOptions* options, FFCPUResult* cpu)
             }
         }
     }
-    cpu->frequencyMax /= 1000;
 
     int clockRate = ffSysctlGetInt("hw.clockrate", 0);
-    cpu->frequencyBase = clockRate <= 0 ? 0.0/0.0 : clockRate / 1000.0;
+    cpu->frequencyBase = clockRate <= 0 ? 0 : (uint32_t) (clockRate / 1000);
     cpu->temperature = FF_CPU_TEMP_UNSET;
 
     if (options->temp)
